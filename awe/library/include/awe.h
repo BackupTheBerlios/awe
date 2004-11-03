81520b18183ece9bb6641aa9be37a8cf99283b18
qbasicer <qbasicer>
2004-11-03 19:43:19 +0000

    Added my awe.h

---
+++ awe/library/include/awe.h
@@ -0,0 +1,28 @@
+#ifndef AWE_H
+#define AWE_H
+
+#include "input.h"
+#include "gdi.h"
+#include "font.h"
+#include "mouse.h"
+#include "com.h"
+#include "skin.h"
+#include "widget.h"
+#include "event.h"
+#include "dataobjects.h"
+#include "draganddrop.h"
+#include "geomman.h"
+#include "control.h"
+#include "pushbutton.h"
+
+#ifdef __cplusplus
+   extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+   }
+#endif
+
+
+#endif //AWE_H
